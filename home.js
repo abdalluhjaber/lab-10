7ca824aedb84eed2f30ec9ae22fd2abbe8fe1138
abdalluhjaber <143411177+abdalluhjaber@users.noreply.github.com>
2023-10-09 20:37:41 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+const nenulcon = document.querySelector('#menu-icon');
+
+nenulcon.addEventListener('click', () => {
+
+    nenulcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+});
---
+++ siune.js
@@ -0,0 +1,14 @@
+const wrapper = document.querySelector('.wrapper');
+const registerlink = document.querySelector('.register-link');
+const loginlink = document.querySelector('.login-link');
+
+registerlink.onclick = () => {
+    wrapper.classList.add('active');
+}
+
+loginlink.onclick = () => {
+    wrapper.classList.remove('active');
+
+}
+
+
